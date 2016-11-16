459254ea62eda007cd8d75ec7c99ccec9758a78f
Rhys Short <rhyshort@uk.ibm.com>
2016-11-16 15:50:30 +0000

    Coallopse else if else into else

--- cloudant-http/src/main/java/com/cloudant/http/interceptors/CookieInterceptor.java
+++ cloudant-http/src/main/java/com/cloudant/http/interceptors/CookieInterceptor.java
@@ -251,12 +251,6 @@ public class CookieInterceptor implements HttpConnectionRequestInterceptor,
                 if (responseCode == 401) {
                     logger.severe("Credentials are incorrect, cookie authentication will not be" +
                             " attempted again by this interceptor object");
-                } else if (responseCode / 100 == 5) {
-                    logger.log(Level.SEVERE,
-                            "Failed to get cookie from server, response code {0}, cookie " +
-                                    "authentication will not be attempted again by this interceptor" +
-                                    " object",
-                            responseCode);
                 } else {
                     // catch any other response code
                     logger.log(Level.SEVERE,
