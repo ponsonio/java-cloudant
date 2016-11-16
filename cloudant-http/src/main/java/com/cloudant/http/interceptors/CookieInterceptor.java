94b5bfb49d206f78897c81888840adf9af3427de
Rhys Short <rhyshort@uk.ibm.com>
2016-11-16 13:27:57 +0000

    Correctly handle CouchDB instances located at a path for auth.
    Correctly handle CouchDB instances located at a path when using
    cookie authentication.
    
    Fixes #320

--- cloudant-http/src/main/java/com/cloudant/http/interceptors/CookieInterceptor.java
+++ cloudant-http/src/main/java/com/cloudant/http/interceptors/CookieInterceptor.java
@@ -59,22 +59,27 @@ public class CookieInterceptor implements HttpConnectionRequestInterceptor,
     private final byte[] sessionRequestBody;
     private final CookieManager cookieManager = new CookieManager();
     private final AtomicBoolean shouldAttemptCookieRequest = new AtomicBoolean(true);
+    private final URL sessionURL;
 
     /**
      * Constructs a cookie interceptor.
-     *
-     * @param username The username to use when getting the cookie
+     *  @param username The username to use when getting the cookie
      * @param password The password to use when getting the cookie
+     * @param baseURL
      */
-    public CookieInterceptor(String username, String password) {
+    public CookieInterceptor(String username, String password, String baseURL) {
 
         try {
+            this.sessionURL = new URL(String.format("%s/_session", baseURL));
             username = URLEncoder.encode(username, "UTF-8");
             password = URLEncoder.encode(password, "UTF-8");
             this.sessionRequestBody = String.format("name=%s&password=%s", username, password)
                     .getBytes("UTF-8");
         } catch (UnsupportedEncodingException e) {
             //all JVMs should support UTF-8, so this should not happen
+            throw new RuntimeException(e);
+        } catch (MalformedURLException e) {
+            // this should be a valid URL since the builder is passing it in
             throw new RuntimeException(e);
         }
     }
@@ -88,7 +93,7 @@ public class CookieInterceptor implements HttpConnectionRequestInterceptor,
         // First time we will have no cookies
         if (cookieManager.getCookieStore().getCookies().isEmpty() && shouldAttemptCookieRequest
                 .get()) {
-            if (!requestCookie(connection.getURL(), context)) {
+            if (!requestCookie(context)) {
                 // Requesting a cookie failed, set a flag if we failed so we won't try again
                 shouldAttemptCookieRequest.set(false);
             }
@@ -185,7 +190,7 @@ public class CookieInterceptor implements HttpConnectionRequestInterceptor,
 
                     if (renewCookie) {
                         logger.finest("Cookie was invalid attempt to get new cookie.");
-                        boolean success = requestCookie(connection.getURL(), context);
+                        boolean success = requestCookie(context);
                         if (success) {
                             // New cookie obtained, replay the request
                             context.replayRequest = true;
@@ -207,13 +212,8 @@ public class CookieInterceptor implements HttpConnectionRequestInterceptor,
 
     }
 
-    private boolean requestCookie(URL url, HttpConnectionInterceptorContext context) {
+    private boolean requestCookie(HttpConnectionInterceptorContext context) {
         try {
-            URL sessionURL = new URL(String.format("%s://%s:%d/_session",
-                    url.getProtocol(),
-                    url.getHost(),
-                    url.getPort()));
-
             HttpConnection conn = Http.POST(sessionURL, "application/x-www-form-urlencoded");
             conn.setRequestBody(sessionRequestBody);
 
